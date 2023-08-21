0cb9d50be6751b707128b613e0ccbd7d6393060c
Mikhail Moskovkin <anstane228@gmail.com>
2023-08-21 19:58:23 +0300

    Models + Admin

--- project/models/admin.py
+++ project/models/admin.py
@@ -1,1 +1,50 @@
 from django.contrib import admin
+
+from .models import (
+    Musician,
+    Album,
+    Song
+)
+
+
+@admin.register(Musician)
+class MusicianAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+    )
+    search_fields = (
+        'name',
+    )
+
+
+@admin.register(Album)
+class AlbumAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'musician',
+        'year',
+    )
+    list_filter = (
+        'musician',
+        'year',
+    )
+    search_fields = (
+        'name',
+        'musician',
+        'year',
+    )
+
+
+@admin.register(Song)
+class SongAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'get_albums',
+    )
+    search_fields = (
+        'name',
+        'album',
+    )
+
+    def get_albums(self, obj):
+        return '\n'.join([a.name for a in obj.album.all()])
--- project/models/models.py
+++ project/models/models.py
@@ -1,2 +1,50 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
+
+class Musician(models.Model):
+    """Класс модели музыканта."""
+
+    name = models.CharField(
+        max_length=150, unique=True
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Album(models.Model):
+    """Класс модели альбома."""
+
+    name = models.CharField(
+        max_length=150, unique=True
+    )
+    musician = models.ForeignKey(
+        Musician,
+        related_name='album',
+        on_delete=models.CASCADE
+    )
+    year = models.PositiveIntegerField(
+        validators=[
+            MinValueValidator(1),
+            MaxValueValidator(2023)
+        ]
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Song(models.Model):
+    """Класс модели песни."""
+
+    name = models.CharField(
+        max_length=150, unique=True
+    )
+    album = models.ManyToManyField(
+        Album,
+        related_name='song'
+    )
+
+    def __str__(self):
+        return self.name
